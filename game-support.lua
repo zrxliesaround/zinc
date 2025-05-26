986c395c33b0f0aecc8c55f9301d0dd554b0da72
zrxliesaround <goneissigma@gmail.com>
2025-05-26 15:54:18 -0500

    Create game-support.lua

---
+++ game-support.lua
@@ -0,0 +1,153 @@
+return {
+    -- Official & Popular Da Hood Games
+    [2788229376] = {
+        name = "Da Hood",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "MainEvent",
+            Shoot = "ShootEvent"
+        }
+    },
+
+    [66654135] = {
+        name = "Derhood",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "DerEvent",
+            Shoot = "DerShoot"
+        }
+    },
+
+    [99234567] = {
+        name = "Deehood",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "DeeEvent",
+            Shoot = "DeeShoot"
+        }
+    },
+
+    [88001234] = {
+        name = "Hood Customs",
+        safe = true,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "CustomsHit",
+            Shoot = "CustomsShoot"
+        }
+    },
+
+    -- Placeholder Copies
+    [100000001] = {
+        name = "Da Hood Copy 1",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitRemote",
+            Shoot = "GunFire"
+        }
+    },
+
+    [100000002] = {
+        name = "Da Hood Copy 2",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "FireEvent",
+            Shoot = "FireGun"
+        }
+    },
+
+    [100000003] = {
+        name = "Da Hood Copy 3",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitPartRemote",
+            Shoot = "RemoteShoot"
+        }
+    },
+
+    [100000004] = {
+        name = "Da Hood Copy 4",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitSomething",
+            Shoot = "GunBang"
+        }
+    },
+
+    [100000005] = {
+        name = "Da Hood Copy 5",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HPart",
+            Shoot = "Fire"
+        }
+    },
+
+    [100000006] = {
+        name = "Urban Warfare RP",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "DamageEvent",
+            Shoot = "ShootProjectile"
+        }
+    },
+
+    [100000007] = {
+        name = "Thug Simulator X",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "CombatRemote",
+            Shoot = "ShootX"
+        }
+    },
+
+    [100000008] = {
+        name = "Da Hood Night",
+        safe = true,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "RemoteDamage",
+            Shoot = "RemoteShoot"
+        }
+    },
+
+    [100000009] = {
+        name = "Hood Skirmish",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitFX",
+            Shoot = "ShootFX"
+        }
+    },
+
+    [100000010] = {
+        name = "Gangsta Life RP",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "HitServer",
+            Shoot = "GunServer"
+        }
+    },
+
+    [100000011] = {
+        name = "Hood Legends",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "LegendHit",
+            Shoot = "LegendShoot"
+        }
+    }
+}
