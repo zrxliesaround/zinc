457f857205e95f85fd626f66324e630fbf2df31d
zrxliesaround <goneissigma@gmail.com>
2025-05-26 16:14:51 -0500

    Update zinc-hub.lua

--- zinc-hub.lua
+++ zinc-hub.lua
@@ -90,17 +90,33 @@ end
 
 -- Speed Modifications
 if config['Speed Modifications'].Options.Enabled then
+    local speedEnabled = true
     local speed = config['Speed Modifications'].Options.DefaultSpeed
+    local toggleKey = config['Speed Modifications'].Options.Keybinds.ToggleMovement:lower()
+    local speedUpKey = config['Speed Modifications'].Options.Keybinds['Speed +5']:lower()
+    local speedDownKey = config['Speed Modifications'].Options.Keybinds['Speed -5']:lower()
+
     RunService.RenderStepped:Connect(function()
         if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
-            LocalPlayer.Character.Humanoid.WalkSpeed = speed
+            if speedEnabled then
+                LocalPlayer.Character.Humanoid.WalkSpeed = speed
+            else
+                LocalPlayer.Character.Humanoid.WalkSpeed = 16 -- default WalkSpeed
+            end
         end
     end)
+
     Mouse.KeyDown:Connect(function(key)
-        if key == config['Speed Modifications'].Options.Keybinds['Speed +5']:lower() then
-            speed += 5
-        elseif key == config['Speed Modifications'].Options.Keybinds['Speed -5']:lower() then
-            speed -= 5
+        key = key:lower()
+        if key == toggleKey then
+            speedEnabled = not speedEnabled
+        end
+        if speedEnabled then
+            if key == speedUpKey then
+                speed = speed + 5
+            elseif key == speedDownKey then
+                speed = math.max(0, speed - 5)
+            end
         end
     end)
 end
